db8da2546cec80dc7186e27d0796ebb6fd3b2bae
Dan Andersen <dandersen2@gmail.com>
2016-02-23 10:40:33 -0500

    Re-add the device token field after heroku reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,61 +10,70 @@ Event.create!(name: 'Job Interview at Apple',
               address: '1 Infinite Loop, Cupertino, California 95014',
               user_email: 'getmovingfinalproject@gmail.com',
               start_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
-              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00')
+              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
+              device_token: "1234556575478768632456458674"
               )
 
 Event.create!(name: 'Job Interview at Google',
               address: '1600 Amphitheatre Parkway in Mountain View, Santa Clara County, California',
               user_email: 'getmovingfinalproject@gmail.com',
               start_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
-              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00')
+              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
+              device_token: "123455657547876213576458674"
               )
 
 Event.create!(name: 'Job Interview at Facebook',
               address: '1 Hacker Way, Palo Alto, California 12345',
               user_email: 'getmovingfinalproject@gmail.com',
               start_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
-              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00')
+              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
+              device_token: "123455657547876868576478974"
               )
 
 Event.create!(name: 'Job Interview at Duane Reade',
               address: '60 Wall street',
               user_email: 'getmovingfinalproject@gmail.com',
               start_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
-              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00')
+              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
+              device_token: "123455654257876868576458674"
               )
 
 Event.create!(name: 'Job Interview at CVS',
               address: '60 Wall street',
               user_email: 'getmovingfinalproject@gmail.com',
               start_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
-              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00')
+              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
+              device_token: "123455657547878968576458674"
               )
 
 Event.create!(name: 'Job Interview at Toys R Us',
               address: '60 Wall street',
               user_email: 'getmovingfinalproject@gmail.com',
               start_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
-              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00')
+              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
+              device_token: "123455564547876868576458674"
               )
 
 Event.create!(name: 'Job Interview at Whole Foods',
               address: '60 Wall street',
               user_email: 'getmovingapp1@gmail.com',
               start_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
-              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00')
+              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
+              device_token: "123452357547876868576458674"
               )
 
 Event.create!(name: 'Job Interview at PlanetFitness',
               address: '60 Wall street',
               user_email: 'getmovingapp1@gmail.com',
               start_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
-              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00')
+              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
+              device_token: "123455657543476834576458674"
               )
 
 Event.create!(name: 'Job Interview at Twitter',
               address: '60 Wall street',
               user_email: 'getmovingapp1@gmail.com',
               start_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
-              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00')
+              departure_time: DateTime.parse('2016-02-22T21:31:46+00:00'),
+              device_token: "123455657547876834576458674"
               )
